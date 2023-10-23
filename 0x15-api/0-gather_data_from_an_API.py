da8705cc36c1ca7821d434ba584068d6a391f443
ZAKARIA <zakariaelaro@gmail.com>
2023-10-23 12:43:03 -0700

    Check taks 0 api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+def fetch_employee_data(employee_id):
+    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+
+    tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    tasks_response = requests.get(tasks_url)
+    tasks_data = tasks_response.json()
+
+    completed_tasks = [task for task in tasks_data if task['completed']]
+    total_tasks = len(tasks_data)
+
+    print(f"Employee {employee_data['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    fetch_employee_data(employee_id)
