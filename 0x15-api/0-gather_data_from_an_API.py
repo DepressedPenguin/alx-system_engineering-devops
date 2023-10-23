2189c3d793c109a90b9d5a1018d62c048386cfe2
ZAKARIA <zakariaelaro@gmail.com>
2023-10-23 16:46:03 -0700

    Check task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,12 +9,20 @@ def fetch_employee_data(employee_id):
     """Function to fetch data"""
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     employee_response = requests.get(employee_url)
+    if employee_response.status_code != 200:
+        print("Employee not found")
+        return
+
     employee_data = employee_response.json()
 
     tasks_url = (
         f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     )
     tasks_response = requests.get(tasks_url)
+    if tasks_response.status_code != 200:
+        print("Tasks not found for this employee")
+        return
+
     tasks_data = tasks_response.json()
 
     completed_tasks = [task for task in tasks_data if task['completed']]
@@ -31,5 +39,10 @@ if __name__ == "__main__":
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    fetch_employee_data(employee_id)
+    try:
+        employee_id = int(sys.argv[1])
+        fetch_employee_data(employee_id)
+    except ValueError:
+        print("Employee ID should be an integer")
+        sys.exit(1)
+
