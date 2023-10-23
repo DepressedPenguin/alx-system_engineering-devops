690e573e19ac9ab61310e5bcc239f85b669c09f5
ZAKARIA <zakariaelaro@gmail.com>
2023-10-23 16:25:05 -0700

    Trying to fix task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,27 @@
 import requests
 import sys
 
+
 def fetch_employee_data(employee_id):
-    """function to fetch data"""
+    """Function to fetch data"""
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     employee_response = requests.get(employee_url)
     employee_data = employee_response.json()
 
-    tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    tasks_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    )
     tasks_response = requests.get(tasks_url)
     tasks_data = tasks_response.json()
 
     completed_tasks = [task for task in tasks_data if task['completed']]
     total_tasks = len(tasks_data)
 
-    print(f"Employee {employee_data['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    print(f"Employee {employee_data['name']} is done with tasks "
+          f"({len(completed_tasks)}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
