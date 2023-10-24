47e1f9d8f276beffdca98029d00efe4b1d9aaf21
ZAKARIA <zakariaelaro@gmail.com>
2023-10-23 17:14:40 -0700

    Check task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,48 +1,14 @@
 #!/usr/bin/python3
-"""Task 0"""
-
+""" data in the CSV format"""
 import requests
 import sys
 
-
-def fetch_employee_data(employee_id):
-    """Function to fetch data"""
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    employee_response = requests.get(employee_url)
-    if employee_response.status_code != 200:
-        print("Employee not found")
-        return
-
-    employee_data = employee_response.json()
-
-    tasks_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    )
-    tasks_response = requests.get(tasks_url)
-    if tasks_response.status_code != 200:
-        print("Tasks not found for this employee")
-        return
-
-    tasks_data = tasks_response.json()
-
-    completed_tasks = [task for task in tasks_data if task['completed']]
-    total_tasks = len(tasks_data)
-
-    print(f"Employee {employee_data['name']} is done with tasks "
-          f"({len(completed_tasks)}/{total_tasks}):")
-    for task in tasks_data:
-        print(f"\t{task['title']}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    try:
-        employee_id = int(sys.argv[1])
-        fetch_employee_data(employee_id)
-    except ValueError:
-        print("Employee ID should be an integer")
-        sys.exit(1)
-
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
