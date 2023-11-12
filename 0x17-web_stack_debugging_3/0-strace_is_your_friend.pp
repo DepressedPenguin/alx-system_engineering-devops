c3a35bbda34fa122279a0b20d428796797785eff
ZAKARIA <zakariaelaro@gmail.com>
2023-11-12 17:33:37 +0000

    trying to fix the task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,2 @@
-exec { 'fix-wordpress':
-  command => 'sed -i s/\.phpp/\.php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/',
-}
+# fix wordpress
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
